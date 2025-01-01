6e2616936b8a5963b5e9115086179dd97edec236
Romain BERGOUT <romain.jimmy.bergout@gmail.com>
2025-01-01 17:29:01 +0100

    Mise a Jour du 01/01/2025

--- Front-End/creation-connection.js
+++ Front-End/creation-connection.js
@@ -1,10 +1,20 @@
-const form1 = document.getElementById("form1");
+const formCreate = document.getElementById("create");
+const formConnect = document.getElementById("connect");
 const tabInput = document.querySelectorAll("input");
 console.log(tabInput);
 const inputTab = Array.from(tabInput);
 console.log(inputTab);
 
-form1.addEventListener("submit", function(){
+formCreate.addEventListener("submit", function(){
+    console.log(inputTab[0].value);
+    console.log(inputTab[1].value);
+    console.log(inputTab[2].value);
+    console.log(inputTab[3].value);
+    console.log(inputTab[4].value);
+    console.log(inputTab[5].value);
+});
+
+formConnect.addEventListener("submit", function(){
     console.log(inputTab[0].value);
     console.log(inputTab[1].value);
     console.log(inputTab[2].value);
